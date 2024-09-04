822b5806a670e04d0cd56b74153cb3bcb11136e8
Leandro SJ <leandro.dejesus@outlook.com.br>
2024-09-04 01:03:11 -0300

    Conhecendo funções Math

---
+++ estrutura_sequencial/Math_function.java
@@ -0,0 +1,18 @@
+package estrutura_sequencial;
+
+public class Math_function {
+    public static void main(String[] args) {
+
+        double x = 3.0;
+        double y = 4.0;
+        double z = -5.0;
+        double A, B, C;
+
+        A = Math.sqrt(x);
+        B = Math.sqrt(y);
+        C = Math.sqrt(25.0);
+
+        System.out.println("Raiz quadrada de " + x + " = " +A);
+        //Continua na próxima noite de estudos...
+    }
+}
